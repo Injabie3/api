47c6f032dfc39584fa72dd720f2545f393d1605d
Injabie3 <injabie3@gmail.com>
2022-03-25 22:24:21 -0700

    api: Add create_app factory

--- injabie3-api/__init__.py
+++ injabie3-api/__init__.py
@@ -13,14 +13,6 @@ from werkzeug.utils import secure_filename as secureFilename
 ALLOWED_EXTENSIONS = [ "png", "jpg", "jpeg" ]
 UPLOAD_FOLDER = "/home/pi/git/webcam/webcam/images/"
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-api = Api(app)
-
-uploadParser = api.parser()
-uploadParser.add_argument('image', location='files',
-                                   type=FileStorage, required=True,
-                                   help="The image to upload.")
 
 def isAllowedFilename(filename):
     dotInName = "." in filename
@@ -28,49 +20,65 @@ def isAllowedFilename(filename):
     return dotInName and allowedExtension
 
 
-@api.route('/hello')
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+def createApp(testConfig=None):
+    app = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    api = Api(app)
 
-@api.route("/cam/<camName>")
-class Webcam(Resource):
-    @api.expect(uploadParser)
-    @api.doc(responses={200: "Image successfully uploaded", 400: "Bad request"})
-    def post(self, camName: str):
-        """Upload a webcam image
-        
-        Parameters
-        ----------
-        camName: The webcam name.
-        """
-        camName = secureFilename(camName)
-        args = uploadParser.parse_args()
-        image = args["image"]
-        if not isAllowedFilename(image.filename):
-            raise BadRequest("Bad filename")
-        # TODO f-string after moving from Python3.5
-        filename = secureFilename(image.filename)
-        pathName = app.config["UPLOAD_FOLDER"] + camName
-        Path(pathName).mkdir(parents=True, exist_ok=True)
-        image.save(os.path.join(pathName, filename))
-        return {"message": "Successfully uploaded as {}".format(filename)}, 200
+    uploadParser = api.parser()
+    uploadParser.add_argument('image', location='files',
+                                       type=FileStorage, required=True,
+                                       help="The image to upload.")
 
-    @api.doc(responses={200: "Camera found", 404: "Camera not found"})
-    def get(self, camName: str):
-        """Get the latest webcam image
+    @api.route('/hello')
+    class HelloWorld(Resource):
+        def get(self):
+            return {'hello': 'world'}
 
-        Parameters
-        ----------
-        camName: The webcam name.
-        """
+    @api.route("/cam/<camName>")
+    class Webcam(Resource):
+        @api.expect(uploadParser)
+        @api.doc(responses={200: "Image successfully uploaded", 400: "Bad request"})
+        def post(self, camName: str):
+            """Upload a webcam image
 
-        camName = secureFilename(camName)
-        listOfFiles = glob.glob(app.config["UPLOAD_FOLDER"] + "{}/*".format(camName))
-        if listOfFiles:
-            latestFile = max(listOfFiles, key=os.path.getctime)
-            return sendFile(latestFile)
-        raise NotFound("Camera not found")
+            Parameters
+            ----------
+            camName: The webcam name.
+            """
+            camName = secureFilename(camName)
+            args = uploadParser.parse_args()
+            image = args["image"]
+            if not isAllowedFilename(image.filename):
+                raise BadRequest("Bad filename")
+            # TODO f-string after moving from Python3.5
+            filename = secureFilename(image.filename)
+            pathName = app.config["UPLOAD_FOLDER"] + camName
+            Path(pathName).mkdir(parents=True, exist_ok=True)
+            image.save(os.path.join(pathName, filename))
+            return {"message": "Successfully uploaded as {}".format(filename)}, 200
+
+        @api.doc(responses={200: "Camera found", 404: "Camera not found"})
+        def get(self, camName: str):
+            """Get the latest webcam image
+
+            Parameters
+            ----------
+            camName: The webcam name.
+            """
+
+            camName = secureFilename(camName)
+            listOfFiles = glob.glob(
+                    app.config["UPLOAD_FOLDER"] + "{}/*".format(camName))
+            if listOfFiles:
+                latestFile = max(listOfFiles, key=os.path.getctime)
+                return sendFile(latestFile)
+            raise NotFound("Camera not found")
+
+    return app
+
+def create_app(test_config=None):
+    return createApp(testConfig=test_config)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
