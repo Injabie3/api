2da02458b4edeba9b56c63aab51844a975683db3
Injabie3 <injabie3@gmail.com>
2022-03-31 19:00:52 -0700

    api: rename module to able to import
    - Oops

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="injabie3-api",
     version="0.0.1",
-    packages=find_packages(),
+    packages=["injabie3api"],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
