9d86d8bcc927023bc9ad9d525ab7be9af5dc1d3f
Injabie3 <injabie3@gmail.com>
2022-04-09 21:02:34 -0700

    api: pin version for Werkzeug and markupsafe

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        "Werkzeug==1.0.1",
+        "markupsafe==2.0.1",
         "Flask==1.1.4",
         "flask-restx==0.5.1",
     ],
