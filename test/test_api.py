e4cdeae3e5bd5772afb3b3637a3fa76de2d3cd5e
Injabie3 <injabie3@gmail.com>
2022-04-16 17:34:43 -0700

    test: add test for existing cam

--- test/test_api.py
+++ test/test_api.py
@@ -1,13 +1,19 @@
+import glob
 import json
+import os
 
 import pytest
+import injabie3api
 from injabie3api import create_app
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 @pytest.fixture()
 def app():
     app = create_app()
     app.config.update({
         "TESTING": True,
+        "UPLOAD_FOLDER": f"{DIR_PATH}/cam/"
     })
 
     # other setup can go here
@@ -37,3 +43,10 @@ def test_get_webcam_non_existent(client):
     assert "message" in response.json
     assert "Camera not found" in response.json["message"]
 
+def test_get_webcam_existent(client):
+    response = client.get("/cam/aCameraThatExists")
+    assert response.status_code == 200
+    listOfFiles = glob.glob(f"{DIR_PATH}/cam/aCameraThatExists/*")
+    latestFile = max(listOfFiles, key=os.path.getctime)
+    fileSize = os.path.getsize(latestFile)
+    assert fileSize == len(response.data)
